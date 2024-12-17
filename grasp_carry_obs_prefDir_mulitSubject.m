400337f7d66efd423b25cc286a62d5700e2fecbc
Ariana <67481189+ari798@users.noreply.github.com>
2024-12-17 17:36:44 -0600

    update loading data

--- grasp_carry_obs_prefDir_mulitSubject.m
+++ grasp_carry_obs_prefDir_mulitSubject.m
@@ -1,76 +1,14 @@
-%% load in data in half
-addpath(genpath("C:\Users\ari798\Documents\GitHub")) %laptop
-%addpath(genpath("C:\Users\somlab\Documents\BCI")) % work desktop
-%session_path = 'T:\SessionData\BCI02\MotorExperiments\BCI02.data.00333'; %317; 331; 333
-session_path = 'T:\SessionData\CRS08\MotorExperiments\CRS08Lab.data.00082';%64; 82
-all_set_files = dir(fullfile(session_path, '*.Set*.bin'));
-pat = ".Set" + digitsPattern(4) + ".";
-%exp_sets_TV = [37:39]; exp_sets_VR = [17:19]; session = 317; type = 'obs';task = 'grasp & carry'; subject = 'C1';
-%exp_sets_TV = [1:2]; exp_sets_VR = [10:11]; session = 331; type = 'obs'; task = 'grasp & carry'; subject = 'C1';
-%exp_sets_TV = [1:3]; exp_sets_VR = [11:13]; session = 333; type = 'obs'; task = 'grasp & carry'; subject = 'C1';
-%exp_sets_TV = [13:14]; exp_sets_VR = [5:6]; session = 64; type = 'obs'; task = 'grasp & carry'; subject = 'P4';
-exp_sets_TV = [1:2]; exp_sets_VR = [11:12]; session = 82; type = 'obs'; task = 'grasp & carry'; subject = 'P4';
-
-% load in TV environment
-ct = 1; exp_set_files = {};
-for fIdx = 1:length(all_set_files)
-    fName = all_set_files(fIdx);
-    set_str = extract(fName.name, pat);
-    if ismember(str2double(set_str{1}(end-4:end-1)), exp_sets_TV)
-        exp_set_files{ct} = fName;
-        ct = ct + 1;       
-    end
-end
-Data = cell(1, length(exp_set_files));
-for fIdx = 1:length(Data)
-    fName = exp_set_files{fIdx};
-    % loads all file paths
-    filepath_TV{fIdx,1} = fullfile(session_path, fName.name);
-end
-TV_Data = prepData('files',filepath_TV);
-
-%TV_Data_1 = prepData('files',filepath_TV(1:length(filepath_TV)/2));
-%TV_Data_2 = prepData('files',filepath_TV((length(filepath_TV)/2)+1:length(filepath_TV)));
-
-
-% load in VR environment
-ct = 1; exp_set_files = {};
-for fIdx = 1:length(all_set_files)
-    fName = all_set_files(fIdx);
-    set_str = extract(fName.name, pat);
-    if ismember(str2double(set_str{1}(end-4:end-1)), exp_sets_VR)
-        exp_set_files{ct} = fName;
-        ct = ct + 1;       
-    end
-end
-Data = cell(1, length(exp_set_files));
-for fIdx = 1:length(Data)
-    fName = exp_set_files{fIdx};
-    % loads all file paths
-    filepath_VR{fIdx,1} = fullfile(session_path, fName.name);
-end
-VR_Data = prepData('files',filepath_VR);
-
-%VR_Data_1 = prepData('files',filepath_VR(1:length(filepath_VR)/2));
-%VR_Data_2 = prepData('files',filepath_VR((length(filepath_VR)/2)+1:length(filepath_VR)));
-
-% load split data
-
-TV_Data_1 = prepData('files',filepath_TV(1:length(filepath_TV)/2));
-TV_Data_2 = prepData('files',filepath_TV((length(filepath_TV)/2)+1:length(filepath_TV)));
-VR_Data_1 = prepData('files',filepath_VR(1:length(filepath_VR)/2));
-VR_Data_2 = prepData('files',filepath_VR((length(filepath_VR)/2)+1:length(filepath_VR)));
+%% load in data
+C1_317_TV = load("C1_session317_TV_observation.mat"); C1_317_VR = load("C1_session317_VR_observation.mat");
+C1_331_TV = load("C1_session331_TV_observation.mat"); C1_331_VR = load("C1_session331_VR_observation.mat");
+C1_333_TV = load("C1_session333_TV_observation.mat"); C1_333_VR = load("C1_session333_VR_observation.mat");
+P4_64_TV = load("P4_session64_TV_observation.mat"); P4_64_VR = load("P4_session64_VR_observation.mat");
+P4_82_TV = load("P4_session82_TV_observation.mat"); P4_82_VR = load("P4_session82_VR_observation.mat");
 
 %% identify good channels
 
 goodChannels = identify_good_channels(VR_Data, TV_Data);
 
-%% load split data
-
-TV_Data_1 = prepData('files',filepath_TV(1:length(filepath_TV)/2));
-TV_Data_2 = prepData('files',filepath_TV((length(filepath_TV)/2)+1:length(filepath_TV)));
-VR_Data_1 = prepData('files',filepath_VR(1:length(filepath_VR)/2));
-VR_Data_2 = prepData('files',filepath_VR((length(filepath_VR)/2)+1:length(filepath_VR)));
 
 %% preferred direction - find angle between vectors
 
